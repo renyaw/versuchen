7dd69c8beb3a61ac5e102d0605228b2aa9223a2b
renyaw <norarenada@students.undip.ac.id>
2023-01-10 18:44:15 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ceritaku</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-sky-400">
+    <div class="container ">
+            <a href="login.php"><img src="img/logo.png" alt="" class="w-56 m-auto mt-32"></a>
+            <p class="text-center text-white">Tekan Gambar untuk Login</p>
+    </div>
+
+</body>
+</html>
