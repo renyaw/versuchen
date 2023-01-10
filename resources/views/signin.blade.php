82403cb5471c22c6068e25ac3cc74ba2c3aac9e8
Aulia Putri <aulptr17@gmail.com>
2023-01-10 19:52:44 +0700

    nyobain pull

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -44,7 +44,9 @@
     </nav>
     <!-- Navbar End -->
 
-
+    <!-- coba pull -->
+    <h1>MASUKditedit</h1>
+    <!-- en coba pull -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
