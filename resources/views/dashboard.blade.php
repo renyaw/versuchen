768083a2bd7e24b5a5e3a8962592034f1db1357d
renyaw <norarenada@students.undip.ac.id>
2023-01-12 22:38:26 +0700

    edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Masuk</title>
+        <title>Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        </head>
+        <script src="https://unpkg.com/feather-icons"></script>
+    </head>
     <body>
     <!-- Navbar -->
 
@@ -57,20 +58,18 @@
         <!-- Gambar End -->
 
         <!-- Box Layanan -->
+
         <div class="row mt-5 d-flex justify-content-center">
-            <div class="col-3 ps-4">
-                <div class="card" style="">
-                <div class="card-body text-center">
-                    <p class="card-text">Surat Pengantar</p>
-                    <p class="card-text fw-bold">Domisili Tempat Tinggal</p>
-                </div>
-                </div>
+        <div class="col-3">
+            <div class="logos d-flex justify-content-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="#87CEFA" class="bi bi-currency-dollar border rounded d-flex justify-content-center p-2" viewBox="0 2 16 14">
+                    <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                </svg>
             </div>
 
-            <div class="col-3">
-                <a href="Usaha" class="">
+                <a href="Usaha" class="text-decoration-none text-dark">
                     <div class="card" style="">
-                    <div class="card-body text-center">
+                    <div class="card-body text-center ">
                         <p class="card-text">Surat Keterangan</p>
                         <p class="card-text fw-bold">Domisili Usaha</p>
                     </div>
@@ -78,6 +77,18 @@
                 </a>
 
             </div>
+
+            <div class="col-3 ps-4">
+
+                <div class="card" style="">
+                <div class="card-body text-center">
+                    <p class="card-text">Surat Permohonan </p>
+                    <p class="card-text fw-bold">Kartu Izin Tinggal Terbatas</p>
+                </div>
+                </div>
+            </div>
+
+
             <div class="col-3">
                 <div class="card" style="">
                 <div class="card-body text-center">
