85d30e04462aafaaa6f66e88d67d9c740f5a7c72
renyaw <norarenada@students.undip.ac.id>
2023-01-17 00:58:03 +0700

    add file kelurahan and status

---
+++ resources/views/masyarakat/status.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Status</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://unpkg.com/feather-icons"></script>
+    </head>
+    <body>
+    <!-- Navbar -->
+
+
+    <!-- Navbar End -->
+
+    <!-- Content -->
+    <div class="container mt-4">
+    <div class="card p-2">
+        <h5 class="card-header">Featured</h5>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+
+        </div>
+        <div class="card-footer">
+            <div class="row">
+                <div class="col-8">
+                    <p class="card-text">Wing wing wing wing wing</p>
+                </div>
+                <div class="col-4 d-flex justify-content-end">
+                    <a href="#" class="btn btn-outline-warning">Go somewhere</a>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+    </div>
+
+
+    <!-- Content End -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -24,43 +24,68 @@ Route::get('/signup', function () {
 Route::get('/beranda', function () {
     return view('masyarakat/beranda');
 });
+
 Route::get('/sktm1', function () {
     return view('masyarakat/sktm1');
 });
+
 Route::get('/sktm2', function () {
     return view('masyarakat/sktm2');
 });
+
 Route::get('/dashboardpeg', function () {
     return view('kelurahan/dashboardpeg');
 });
+
 Route::get('/usaha', function () {
     return view('masyarakat/usaha');
 });
+
 Route::get('/domisili', function () {
     return view('masyarakat/domisili');
 });
+
 Route::get('/berkas', function () {
     return view('masyarakat/berkas');
 });
+
 Route::get('/verifikasi', function () {
     return view('kelurahan/verifikasi');
 });
+
 Route::get('/berhasil', function () {
     return view('masyarakat/berhasil');
 });
+
 Route::get('/datadiri', function () {
     return view('masyarakat/datadiri');
 });
+
 Route::get('/kitas', function () {
     return view('masyarakat/kitas');
 });
+
 Route::get('/navbar', function () {
     return view('navbar');
 });
+
 Route::get('/lihatberkas', function () {
     return view('/kelurahan/lihatberkas');
 });
+
 Route::get('/status', function () {
     return view('masyarakat/status');
+});
+
+Route::get('/lihatdomisili', function () {
+    return view('kelurahan/lihatdomisili');
+});
+
+Route::get('/lihatsktm', function () {
+    return view('kelurahan/lihatsktm');
+});
+
+Route::get('/lihatkitas', function () {
+    return view('kelurahan/lihatkitas');
 });
 
