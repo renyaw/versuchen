356ac0eff50629c4dcce1ce7c975d4d62e2225c8
renyaw <norarenada@students.undip.ac.id>
2023-01-16 14:15:04 +0700

    Tambah gambar dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,14 +61,14 @@
 
         <div class="row mt-5 d-flex justify-content-center">
         <div class="col-3">
-            <div class="logos d-flex justify-content-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="#87CEFA" class="bi bi-currency-dollar border rounded d-flex justify-content-center p-2" viewBox="0 2 16 14">
-                    <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
-                </svg>
+            <div class="logos d-flex justify-content-center z-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="#87CEFA" class="bi bi-currency-dollar border rounded d-flex justify-content-center p-2" viewBox="0 2 16 14">
+                <path d="M15 .5a.5.5 0 0 0-.724-.447l-8 4A.5.5 0 0 0 6 4.5v3.14L.342 9.526A.5.5 0 0 0 0 10v5.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V14h1v1.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V.5ZM2 11h1v1H2v-1Zm2 0h1v1H4v-1Zm-1 2v1H2v-1h1Zm1 0h1v1H4v-1Zm9-10v1h-1V3h1ZM8 5h1v1H8V5Zm1 2v1H8V7h1ZM8 9h1v1H8V9Zm2 0h1v1h-1V9Zm-1 2v1H8v-1h1Zm1 0h1v1h-1v-1Zm3-2v1h-1V9h1Zm-1 2h1v1h-1v-1Zm-2-4h1v1h-1V7Zm3 0v1h-1V7h1Zm-2-2v1h-1V5h1Zm1 0h1v1h-1V5Z"/>
+            </svg>
             </div>
 
                 <a href="Usaha" class="text-decoration-none text-dark">
-                    <div class="card" style="">
+                    <div class="card z-1" style="">
                     <div class="card-body text-center ">
                         <p class="card-text">Surat Keterangan</p>
                         <p class="card-text fw-bold">Domisili Usaha</p>
@@ -81,7 +81,7 @@
             <div class="col-3 ps-4">
             <div class="logos d-flex justify-content-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="#87CEFA" class="bi bi-currency-dollar border rounded d-flex justify-content-center p-2" viewBox="0 2 16 14">
-                    <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
                 </svg>
             </div>
                 <div class="card" style="">
@@ -96,7 +96,7 @@
             <div class="col-3">
             <div class="logos d-flex justify-content-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="#87CEFA" class="bi bi-currency-dollar border rounded d-flex justify-content-center p-2" viewBox="0 2 16 14">
-                    <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z"/>
                 </svg>
             </div>
                 <div class="card" style="">
@@ -107,6 +107,11 @@
                 </div>
                 </div>
             <div class="col-3">
+            <div class="logos d-flex justify-content-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="#87CEFA" class="bi bi-currency-dollar border rounded d-flex justify-content-center p-2" viewBox="0 2 16 14">
+                    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z"/>
+                </svg>
+            </div>
             <div class="card" style="">
                 <div class="card-body text-center">
                     <p class="card-text">Kumpulan Berkas</p>
