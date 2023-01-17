63fab760686296bc2a81ac1eb316f565e2e8d13e
taskiaintan <111947053+taskiaintan@users.noreply.github.com>
2023-01-17 16:17:41 +0700

    dashboard count kecamatan

---
+++ resources/views/kecamatan/dashkec2.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Masuk</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        </head>
+    <body>
+    <!-- Navbar -->
+
+    <nav class="navbar navbar-expand-lg " style="background-color:#87CEFA;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="signup" style="">
+                <img src="img/Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">Kota Salatiga
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Context -->
+    
+    <div class="container text-center">
+        <div class="row" style="margin-top:90px; margin-bottom:90px; ">
+            <div class="col">
+                <div class="container position-relative" style="width: 19rem; height: 19rem; background-color:#87CEFA; border-radius: 1rem;">  
+                    <div class="position-absolute top-50 start-50 translate-middle">
+                        <p class="fs-1 fw-bold text-light" >22</p>
+                        <p class="text-light" >Kartu Izin Tinggal Terbatas</p>
+                    </div>
+                </div>   
+            </div>
+            <div class="col">
+                <div class="container position-relative" style="width: 19rem; height:19rem; background-color:#87CEFA; border-radius: 1rem;">
+                    <div class="position-absolute top-50 start-50 translate-middle">
+                        <p class="fs-1 fw-bold text-light ">22</p>
+                        <p class="text-light" >Surat Pengantar Domisili Usaha</p>
+                    </div>    
+                </div>
+            </div>   
+            <div class="col">
+                <div class="container position-relative" style="width: 19rem; height:19rem; background-color:#87CEFA; border-radius: 1rem;">
+                    <div class="position-absolute top-50 start-50 translate-middle">
+                        <p class="fs-1 fw-bold text-light">22</p>
+                        <p class="text-light" >Surat Keterangan Tidak Mampu</p>
+                    </div>
+                </div>   
+            </div>
+        </div>
+        <div class="row"> 
+            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                <a href="verifikasi" type="button" class="btn btn-outline-success">Kembali</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Context -->
+
+    <h1></h1>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
