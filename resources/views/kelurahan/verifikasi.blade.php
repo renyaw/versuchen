da23667cad28cb37f73ffc943fcd90bd67d8cf6b
taskiaintan <111947053+taskiaintan@users.noreply.github.com>
2023-01-23 16:46:09 +0700

    insert navbar kelurahan

--- resources/views/kelurahan/verifikasi.blade.php
+++ resources/views/kelurahan/verifikasi.blade.php
@@ -7,43 +7,11 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         </head>
     <body>
+
     <!-- Navbar -->
-
-    <nav class="navbar navbar-expand-lg " style="background-color:#87CEFA;">
-        <div class="container">
-            <a class="navbar-brand" href="#" style="">
-                <img src="img/Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">Kota Salatiga
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown link
-                        </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @extends('navbar.navbaradmin')
     <!-- Navbar End -->
-
+    
     <!-- Content -->
     <div class="container mt-4 d-flex justify-content-start">
         <div class="form-group">
