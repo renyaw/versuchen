527af47bc5ea6bac3f96ee6238ba7e023ce0b053
renyaw <norarenada@students.undip.ac.id>
2023-02-01 02:18:02 +0700

    status model done

--- app/Models/status.php
+++ app/Models/status.php
@@ -8,9 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class status extends Model
 {
     use HasFactory;
-    public function antrean_kredit()
-    {
-        return $this->belongsTo(AntreanKredit::class, 'id_kredit', 'id_kredit');
-    }
+    protected $table = 'status';
+    protected $guarded = [];
 
+    public function status_sktm()
+    {
+        return $this->hasMany(AntreanSKTM::class, 'id_status', 'id_status');
+    }
+    public function status_kredit()
+    {
+        return $this->hasMany(AntreanKredit::class, 'id_status', 'id_status');
+    }
+    public function status_dom()
+    {
+        return $this->hasMany(AntreanDomisili::class, 'id_status', 'id_status');
+    }
 }
